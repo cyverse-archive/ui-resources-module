2a8ea6050c0fd7a138d693ae8450e4a43e1d4f0a
Paul Sarando <psarando@iplantcollaborative.org>
2013-08-27 14:54:58 -0700

    Renamed I18N#RULES to VALIDATION.

--- src/main/java/org/iplantc/core/resources/client/messages/I18N.java
+++ src/main/java/org/iplantc/core/resources/client/messages/I18N.java
@@ -18,7 +18,7 @@ public class I18N {
 
     public static final IplantDisplayStrings DISPLAY = GWT.create(IplantDisplayStrings.class);
 	public static final IplantErrorStrings  ERROR = GWT.create(IplantErrorStrings.class);
-    public static final IplantValidationMessages RULES = GWT.create(IplantValidationMessages.class);
+    public static final IplantValidationMessages VALIDATION = GWT.create(IplantValidationMessages.class);
     public static final IplantContextualHelpStrings HELP = GWT.create(IplantContextualHelpStrings.class);
     public static final IplantValidationConstants V_CONSTANTS = GWT.create(IplantValidationConstants.class);
     public static final IplantNewUserTourStrings TOUR = GWT.create(IplantNewUserTourStrings.class);
